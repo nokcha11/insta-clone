4f68d81cf3f87a09e3492b785b6d8bc769bc88dc
nockcha11 <lie8220@naver.com>
2024-06-11 15:40:44 +0900

    ✨ dark mode

---
+++ script.js
@@ -0,0 +1,22 @@
+// 다크모드 구현하기
+let darkMode = document.querySelector('.darkmode');
+let logo = document.getElementById("logo");
+let loginBtn = document.querySelector('.login-btn');
+console.log('darkMode, logo, loginbtn');
+
+let count=0;
+
+function modeToggle() {
+
+  let modeTxt = darkMode.innerHTML
+  let footer = document.querySelector('footer');
+
+  
+
+
+
+
+}
+
+
+
