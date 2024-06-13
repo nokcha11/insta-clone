798b0327321be957ec9b2c448918a6bc30a290b3
nockcha11 <lie8220@naver.com>
2024-06-13 11:16:51 +0900

    ✨ e.target. return, 매개변수, 삼항연산자

--- teachv.js
+++ teachv.js
@@ -1,56 +1,119 @@
 function updateInputState(input, activeVar) {
-  // trim 공백
-  // 공백 제거한 value 값의 길이가 0보다 클 때
-  if(input.value.trim().length > 0) {
+  // trim() 공백
+  // 공백 제거한 value 값의 길이가 0보다 클 때 => 사용자가 input 창에 입력한 값이 하나라도 있나
+  if (input.value.trim().length > 0) {
     input.parentElement.classList.add('active');
     activeVar = true;
-  }else {
+  } else {
     // value 값의 길이가 없을 때 
     input.parentElement.classList.remove('active');
-    activeVar = true;
+    activeVar = false;
   }
 
-  return activeVar;
+  // return : 해당 함수가 실행된 후, 함수 호출한 위치로 전달되는 값
+  return activeVar; // true , false
 }
 
 let userId = document.getElementById('userid');
-
-
 let userPw = document.getElementById('userpw');
 
 let idActive = false;
 let pwActive = false;
-// let idActive = let pwActive = false;
+// let idActive = pwActive = false;
 
 let submitBtn = document.getElementById('submit-btn');
-// console.log(submitBtn);
-
-
-
-// console.log(userId,userPw);
 
 function handleInput(e) {
   // e.target => 이벤트가 일어나는 대상
   let input = e.target;
   let type = input.getAttribute("type");
-  console.log(type);
-
-  if(type == "text") {
-    idActive =  updateInputState(input, idActive);
-  }else {
-    pwActive = updateInputState(input, pwActive);
+  
+  if (type == "text") {
+    idActive = updateInputState(input, idActive); // return 값 => true, false
+  } else {
+    // type이 text가 아닐 경우 => password
+    pwActive = updateInputState(input, pwActive); // return 값 => true, false
   }
 
-  if(idActive && pwActive) {
+  // idActive랑 pwActive가 모두 참일 때 => input.value.length > 0
+  if (idActive && pwActive) {
     submitBtn.removeAttribute('disabled');
-  }else {
-    submitBtn.setAttribute('disabled',true);
+  } else {
+    // idActive나 pwActive 중 둘 중 하나라도 false 일 때 
+    submitBtn.setAttribute('disabled', true);
   }
-
-
-  // console.log(input);
-  // console.log(e.target);
 }
 
 userId.addEventListener('keyup', handleInput);
 userPw.addEventListener('keyup', handleInput);
+
+// let appTxt = document.querySelector('.app-txt');
+
+// function btnClick (e) {
+//   console.log(e.target)
+// }
+// appTxt.addEventListener('click', btnClick)
+
+
+let pwVisible = document.getElementById('pw-visible');
+
+function pwMode () {
+  // userpw의 type이 password일 경우
+  // 변경대상.setAttribute(어떤속성을, 어떤걸로)
+
+  if(userPw.getAttribute('type') == 'password') {
+    // 1. 비밀번호 표시 클릭 => userpw (input) type => text로 변경
+    userPw.setAttribute('type', 'text');
+    
+    // 2. pwVisible.innerHTML => '숨기기'로 변경
+   pwVisible.innerHTML = '숨기기';
+
+  } else {
+    // userpw의 type이 text일 경우
+    // 1. 숨기기 클릭 => userpw (input) type => password로 변경
+    userPw.setAttribute('type', 'password');
+
+    // 2. pwVisible.innerHTML => '비밀번호 표시'로 변경
+    pwVisible.innerHTML = '비밀번호 표시';
+  }
+
+
+}
+
+pwVisible.addEventListener('click', pwMode)
+// console.log(pwVisible);
+
+
+//  dark mode / light mode
+let modeBtn = document.getElementById('mode-toggle');
+
+function modeToggle(e) {
+  // 클릭시 a태그 이동하면서 깜빡 거리는것 없애는 것 
+  e.preventDefault();
+
+
+// 1. body 태그에 dark 라는 class toggle
+// dark 라는 클래스가 있으면 remove, 없으면 add
+let body = document.querySelector('body');
+body.classList.toggle('dark');
+
+  // body에 dark라는 클래스가 있을 때
+  // modeBtn.innerHTML = 'lightmode'
+  // classList.contains()
+
+// if 조건문 ver
+//   if(body.classList.contains('dark')) {
+//     modeBtn.innerHTML = 'Lightmode';
+//   } else {
+//     // body에 light라는 클래스가 있을 때
+//     // modeBtn.innerHTML = 'darktmode'
+//     modeBtn.innerHTML = 'Darkmode';
+//   }
+
+// 삼항연산자 ver
+// 조건 ? 참일 때 : 거짓일 때
+  modeBtn.innerHTML = body.classList.contains('dark') ? 'Lightmode' : 'Darkmode'
+ }
+
+
+modeBtn.addEventListener('click', modeToggle);
